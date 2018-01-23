7c6fd891c814b9264008d0b0bc1bc06b33d63b34
Vanessa Toro <vanetoro84@gmail.com>
2018-01-23 19:41:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(arr){
+  for(var i = 0; i<25;i++){
+    arr.push(`I am ${i} strange loop`);
+  }
+  return arr;
+}
+
+function whileLoop(num){
+  var countdown = num;
+
+  while(countdown > 0){
+  countdown--;
+  console.log(countdown);
+}
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(arr){
+  do{
+    maybeTrue();
+  }
+  while((maybeTrue()=== false) || (arr.length = 0))
+  arr.pop();
+}
